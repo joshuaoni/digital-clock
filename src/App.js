2a4ff52dfc7b107cb846138e722e3d2bcd7aa2e7
joshua <joshuadavidoni@yahoo.com>
2022-06-25 16:00:49 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+	const [seconds, setSeconds] = useState('');
+	const [minutes, setMinutes] = useState('');
+	const [hours, setHours] = useState('');
+	const [day, setDay] = useState('');
+	const [month, setMonth] = useState('');
+	const [year, setYear] = useState('');
+
+	// Updates the time state
+	const updateClock = () => {
+		const now = new Date();
+		if (now.getSeconds().toString().length === 1) {
+			setSeconds('0' + now.getSeconds());
+		} else {
+			setSeconds(now.getSeconds());
+		}
+		if (now.getMinutes().toString().length === 1) {
+			setMinutes('0' + now.getMinutes());
+		} else {
+			setMinutes(now.getMinutes());
+		}
+		if (now.getHours().toString().length === 1) {
+			setHours('0' + now.getHours());
+		} else {
+			setHours(now.getHours());
+		}
+		if (now.getDate().toString().length === 1) {
+			setDay('0' + now.getDate());
+		} else{
+			setDay(now.getDate());
+		} if (now.getMonth().toString().length === 1) {
+			setMonth('0' + now.getMonth());
+		} else {
+			setMonth(now.getMonth());
+		}
+		setYear(now.getFullYear());
+	}
+
+	// Runs the updateClock() function every second
+	const setClock = () => {
+		updateClock();
+		setTimeout(() => {
+			setClock();
+		}, 1000);
+	}
+
+	// Runs the setClock() function anytime the window loads
+	window.onload = function () {
+		setClock();
+	}
+
+
+	return (
+		<div className='App'>
+			<div className='title'>
+				<h3>QWERTY THOUGHTS</h3>
+			</div>
+			<div className='digital'>
+				<div className='date'>
+					<h2>Day - Month - Year</h2>
+					<div>
+						<span>{day}</span>
+						<span>-</span>
+						<span>{month}</span>
+						<span>-</span>
+						<span>{year}</span>
+					</div>
+				</div>
+				<div className='time'>
+					<h2>Hrs - Min - Secs</h2>
+					<div>
+						<span>{hours}</span>
+						<span>-</span>
+						<span>{minutes}</span>
+						<span>-</span>
+						<span>{seconds}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
